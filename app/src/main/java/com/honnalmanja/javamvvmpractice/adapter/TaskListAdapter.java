91674b9e3868d4f19cd68736fe2bfb7327ae7b9a
honnalmanja <37094232+honnalmanja@users.noreply.github.com>
2020-10-12 21:31:10 +0530

    added task related adapter and view holder

---
+++ app/src/main/java/com/honnalmanja/javamvvmpractice/adapter/TaskListAdapter.java
@@ -0,0 +1,24 @@
+package com.honnalmanja.javamvvmpractice.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskListAdapter extends RecyclerView.Adapter<TaskListViewHolder> {
+    @NonNull
+    @Override
+    public TaskListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/honnalmanja/javamvvmpractice/adapter/TaskListViewHolder.java
@@ -0,0 +1,13 @@
+package com.honnalmanja.javamvvmpractice.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskListViewHolder extends RecyclerView.ViewHolder {
+
+    public TaskListViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
